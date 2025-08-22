9d4f9b435aa0411af5b5693108e0e0ea336ca044
Endlessblack <aeiou123321@gmail.com>
2025-08-22 17:12:40 +0800

    Animate realtime overflow

--- overlay.py
+++ overlay.py
@@ -481,7 +481,7 @@ class SubtitleOverlay(QtWidgets.QLabel):
         self._visible_lines = visible
         added = max(0, len(visible) - len(prev))
         if added == 0 and len(visible) == len(prev):
-            for i in range(1, len(prev)):
+            for i in range(1, len(prev) + 1):
                 if visible[:-i] == prev[i:]:
                     added = i
                     break
